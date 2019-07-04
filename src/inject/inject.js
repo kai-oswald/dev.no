dce2cf578d2140dd2f0245294603a8696c9063e0
kai-oswald <kaos1910@gmail.com>
2019-07-04 12:14:50 +0200

    Remove console.logs and non-existent function

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,6 @@ chrome.extension.sendMessage({}, function(response) {
   var readyStateCheckInterval = setInterval(function() {
     if (document.readyState === "complete") {
       clearInterval(readyStateCheckInterval);
-      init();
       chrome.storage.sync.get(
         {
           tags: []
@@ -25,9 +24,6 @@ function hideIgnoredTags(tags) {
       // the whole article is located 2 elements above (parent.parent);
       var articleElement = tagElement.parentElement.parentElement;
       articleElement.style.display = "none";
-
-      console.log("Hid element for tag " + tag);
-      console.log(articleElement);
     });
   });
 }
